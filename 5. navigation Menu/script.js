2a0651d53ef3295daf88c7ace7e0294bdf1bbc74
Sumit-sarkar <sumitopticalit89@gmail.com>
2024-07-01 18:25:14 +0600

    navigation menu

---
+++ 5. navigation Menu/script.js
@@ -0,0 +1,18 @@
+
+const menuBtn = document.querySelectorAll(".menu-btn");
+const nav = document.getElementById("nav");
+const open = document.querySelectorAll(".open");
+const close = document.querySelectorAll(".close");
+
+open[0].addEventListener("click", () =>{
+    nav.style.left = "0";
+    open[0].style.display = "none";
+    close[0].style.display = "block"
+})
+
+close[0].addEventListener("click", () =>{
+    nav.style.left = "-100%";
+     open[0].style.display = "block";
+    close[0].style.display = "none"
+
+})
