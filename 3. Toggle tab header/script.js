a6c3c485a7165f68ae9073e16fdf715f4483974a
Sumit-sarkar <sumitopticalit89@gmail.com>
2024-11-18 17:12:51 +0600

    toggle tab header

--- 3. Toggle tab header/script.js
+++ 3. Toggle tab header/script.js
@@ -1,13 +1,18 @@
 function showpanel(a,b,c){
+    //tabpanel + for loop
     const tabpanel = document.getElementsByClassName("tabpanel");
     for(let i = 0; i< tabpanel.length; i++){
         tabpanel[i].style.display = "none";
     }
 
+    //btn + for loop
     const btn = document.getElementsByClassName("btn");
     for(let i = 0; i< btn.length; i++){
         btn[i].style.backgroundColor = "";
     }
+    //a = onclick(btn)
+    //b = onclick(this)
+    //c = onclick(color name)
     document.getElementById(a).style.display = "block";
     b.style.backgroundColor = c;
 }
