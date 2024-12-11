0012c486519e0073a4da90d9f302ed9ec02c1455
Sumit-sarkar <sumitopticalit89@gmail.com>
2024-12-12 01:17:54 +0600

    project 18: search filter

---
+++ 18. search filter Api/script.js
@@ -0,0 +1,41 @@
+function searchOn() {
+    const input = document.getElementById('find').value.toUpperCase();; 
+    // const filter = input.value.toUpperCase(); 
+    const products = document.getElementById('products'); 
+    const productCards = products.querySelectorAll('.product-card'); 
+    
+    // Loop through each product card
+    productCards.forEach((a) => {
+        const h3 = a.querySelector('.heading'); 
+        const txtValue = h3.textContent || h3.innerText; 
+            if (txtValue.toUpperCase().indexOf(input) > -1) {
+                a.style.display = '';
+            } else {
+                a.style.display = 'none';
+            }
+        }
+    );
+};
+const mainImages = document.querySelectorAll(".product-card img");
+const modalBody = document.querySelector(".modal");
+const modalImages = document.querySelector(".modal-img");
+const text = document.querySelector(".modal-text");
+const close = document.querySelector(".closed");
+
+//take ForEach loop in images to appear a classlist on their
+
+mainImages.forEach((image) =>{
+    image.addEventListener('click', () =>{
+        modalBody.classList.add("active");
+          
+        // //indevidual Title and individual photo
+        modalImages.src = image.src;
+        text.innerHTML = image.alt; 
+        
+
+        close.addEventListener('click', () =>{
+            modalBody.classList.remove("active");
+        });
+    });
+
+});
